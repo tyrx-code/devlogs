6da9fcab44ad402feaa3c3b8e5a26789ff1a01ea
Tomas Gonzalez <tomasgonzalez@MacBook-Air-de-Tomas.local>
2026-04-09 19:43:19 -0600

    Adding auth button to swagger

--- src/main/kotlin/com/tomas/devlogs/config/OpenApiConfig.kt
+++ src/main/kotlin/com/tomas/devlogs/config/OpenApiConfig.kt
@@ -2,6 +2,9 @@ package com.tomas.devlogs.config
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,12 +12,26 @@ import org.springframework.context.annotation.Configuration
 class OpenApiConfig {
     @Bean
     fun customOpenAPI(): OpenAPI {
+        val securitySchemeName = "bearerAuth"
+
         return OpenAPI()
             .info(
                 Info()
                     .title("DevLogs API")
                     .version("1.0")
                     .description("REST API for managing developer log entries")
+            )
+            .addSecurityItem(SecurityRequirement().addList(securitySchemeName))
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        securitySchemeName,
+                        SecurityScheme()
+                            .name(securitySchemeName)
+                            .type(SecurityScheme.Type.HTTP)
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                    )
             )
     }
 }
